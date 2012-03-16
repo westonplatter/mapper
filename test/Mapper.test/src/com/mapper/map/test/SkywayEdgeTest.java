b0f1ba70f234c8b7c643fcba6edaf7bc3eb9c113
desilva <desilva@cs.umn.edu>
2012-03-15 22:40:37 -0500

    Added in Ant Build for generating EMMA instrumentation.

---
+++ test/Mapper.test/src/com/mapper/map/test/SkywayEdgeTest.java
@@ -0,0 +1,92 @@
+//SkywayEdgeTest.java
+/**
+ * Copyright 2012 desilva
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *    
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mapper.map.test;
+
+import junit.framework.TestCase;
+
+/**
+ * @author desilva
+ *
+ */
+public class SkywayEdgeTest extends TestCase
+{
+
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+    }
+
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception
+    {
+        super.tearDown();
+    }
+
+    /**
+     * Test method for {@link com.mapper.map.SkywayEdge#getFirstNode()}.
+     */
+    public void testGetFirstNode()
+    {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.mapper.map.SkywayEdge#getSecondNode()}.
+     */
+    public void testGetSecondNode()
+    {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.mapper.map.SkywayEdge#getDistance()}.
+     */
+    public void testGetDistance()
+    {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.mapper.map.SkywayEdge#getUniqueID()}.
+     */
+    public void testGetUniqueID()
+    {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.mapper.map.SkywayEdge#setUniqueID(int)}.
+     */
+    public void testSetUniqueID()
+    {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.mapper.map.SkywayEdge#equals(java.lang.Object)}.
+     */
+    public void testEqualsObject()
+    {
+        fail("Not yet implemented");
+    }
+
+}
